c248dfe6fd55c2b8ff2e1f602189ad3b133b8860
Ilze Rigerte <irigerte@gmail.com>
2024-05-26 13:47:16 +0300

    heroku default links added

--- src/main/java/com/example/demo/InvoiceManagerApplication.java
+++ src/main/java/com/example/demo/InvoiceManagerApplication.java
@@ -33,7 +33,7 @@ public class InvoiceManagerApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000", "https://invoice-app-frontend-95342-29caa4a707ee.herokuapp.com", "www.invoice-mng.com", "invoice-mng.com", "https://www.invoice-mng.com"));
+		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000", "https://invoice-app-frontend-95342-29caa4a707ee.herokuapp.com", "www.invoice-app-frontend-95342-29caa4a707ee.herokuapp.com", "invoice-app-frontend-95342-29caa4a707ee.herokuapp.com", "www.invoice-mng.com", "invoice-mng.com", "https://www.invoice-mng.com"));
 		//corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin", "Accept", "X-Requested-With",
